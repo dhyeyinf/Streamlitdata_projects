180c6eb32e5c2f27db23a7b9601a868e40ff63e9
Dhyey Findoriya <131277481+dhyeyinf@users.noreply.github.com>
2024-09-28 10:22:16 +0530

    Update p1.py

--- Bio-Informatics DNA Count/p1.py
+++ Bio-Informatics DNA Count/p1.py
@@ -7,9 +7,6 @@ import streamlit as st
 import altair as alt
 from PIL import Image
 
-######################
-# Page Title
-######################
 
 image = Image.open('dna-logo.jpg')
 
@@ -24,16 +21,13 @@ This app counts the nucleotide composition of query DNA!
 """)
 
 
-######################
-# Input Text Box
-######################
 
-#st.sidebar.header('Enter DNA sequence')
+
+
 st.header('Enter DNA sequence')
 
 sequence_input = ">DNA Query 2\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG\nATCTTCCAGACGTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATGC\nTGAACCCGATACATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCCT"
 
-#sequence = st.sidebar.text_area("Sequence input", sequence_input, height=250)
 sequence = st.text_area("Sequence input", sequence_input, height=250)
 sequence = sequence.splitlines()
 sequence = sequence[1:] # Skips the sequence name (first line)
@@ -68,14 +62,12 @@ X = DNA_nucleotide_count(sequence)
 
 X
 
-### 2. Print text
 st.subheader('2. Print text')
 st.write('There are  ' + str(X['A']) + ' adenine (A)')
 st.write('There are  ' + str(X['T']) + ' thymine (T)')
 st.write('There are  ' + str(X['G']) + ' guanine (G)')
 st.write('There are  ' + str(X['C']) + ' cytosine (C)')
 
-### 3. Display DataFrame
 st.subheader('3. Display DataFrame')
 df = pd.DataFrame.from_dict(X, orient='index')
 df = df.rename({0: 'count'}, axis='columns')
@@ -83,7 +75,6 @@ df.reset_index(inplace=True)
 df = df.rename(columns = {'index':'nucleotide'})
 st.write(df)
 
-### 4. Display Bar Chart using Altair
 st.subheader('4. Display Bar chart')
 p = alt.Chart(df).mark_bar().encode(
     x='nucleotide',
